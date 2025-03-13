cc89d649f3de33020ac10679c94497199e4c8f53
Leeroy605 <149532097+Leeroy605@users.noreply.github.com>
2025-03-13 12:52:10 +0000

    Created function to get computers choice for the round.
    Made the getComputerChoice function that uses a random number to make a choice of the three possible hands.
    Result is then stored in the computerChoice variable and displayed via console.log.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
 console.log("Hello World");
+
+let randomNumber = Math.floor(Math.random()*100)
+let computerChoice = getComputerChoice();
+
+
+
+function getComputerChoice () {
+    if (randomNumber <= 33) {
+        return "rock";
+    } else if (randomNumber >= 34 && randomNumber <= 66) {
+        return "paper";
+    } else if (randomNumber > 67) {
+        return "scissors";
+    }
+}
+
+console.log(randomNumber);
+console.log(computerChoice);
