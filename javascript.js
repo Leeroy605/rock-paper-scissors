c0620f773509781137fcd23892d031a6bbe2d274
Leeroy605 <149532097+Leeroy605@users.noreply.github.com>
2025-03-13 18:58:06 +0000

    Started to create playGame function Made playGame function, the game currently works for one round but does not keep track of scores or rounds played.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,6 @@
 
 let randomNumber = Math.floor(Math.random()*100)
-let computerChoice = getComputerChoice();
-let playerChoice = getPlayerChoice();
-let roundResult = playRound(playerChoice, computerChoice);
-let playerScore = 0;
-let computerScore = 0;
+let display = playGame();
 
 
 
@@ -23,37 +19,48 @@ function getPlayerChoice () {
     return playerChoice;
 }
 
-function playGame () {
-
-}
-
-function playRound(playerChoice, computerChoice) {
-    if (playerChoice === computerChoice) {
-        return "Both chose the same. Round is a tie.";
-    } else if ((playerChoice === "paper") && (computerChoice === "rock")) {
-        playerScore++;
-        return "Paper beats rock, you win!"
-    } else if ((playerChoice === "rock") && (computerChoice === "paper")) {
-        computerScore++;
-        return "Paper beats rock, you lose."
-    } else if ((playerChoice === "paper") && (computerChoice === "scissors")) {
-        computerScore++
-        return "Scissors beats paper, you lose."
-    } else if ((playerChoice === "scissors") && (computerChoice === "paper")) {
-        playerScore++;
-        return "Scissors beats paper, you win!"
-    } else if ((playerChoice === "rock") && (computerChoice === "scissors")) {
-        playerScore++;
-        return "Rock beats scissors, you win!"
-    } else if ((playerChoice === "scissors") && (computerChoice === "rock")) {
-        computerScore++;
-        return "Rock beats scissors, you lose."
+function playGame() {
+    let roundsPlayed = 0;
+    for (roundsPlayed < 5; roundsPlayed++;);
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice();
+    let computerScore = 0;
+    let playerScore = 0;
+    function playRound(playerChoice, computerChoice) {
+        if (playerChoice === computerChoice) {
+            return "Both chose the same. Round is a tie.";
+        } else if ((playerChoice === "paper") && (computerChoice === "rock")) {
+            playerScore++
+            return "Paper beats rock, you win!";
+        } else if ((playerChoice === "rock") && (computerChoice === "paper")) {
+            computerScore++
+            return "Paper beats rock, you lose.";
+        } else if ((playerChoice === "paper") && (computerChoice === "scissors")) {
+            computerScore++
+            return "Scissors beats paper, you lose.";
+        } else if ((playerChoice === "scissors") && (computerChoice === "paper")) {
+            playerScore++
+            return "Scissors beats paper, you win!";
+        } else if ((playerChoice === "rock") && (computerChoice === "scissors")) {
+            playerScore++
+            return "Rock beats scissors, you win!";
+        } else if ((playerChoice === "scissors") && (computerChoice === "rock")) {
+            computerScore++
+            return "Rock beats scissors, you lose.";
+        }
     }
-}
-    
-console.log(randomNumber);
+    let roundResult = playRound(playerChoice, computerChoice);
+
+
 console.log(computerChoice);
 console.log(playerChoice);
 console.log(roundResult);
-console.log(playerScore);
-console.log(computerScore);
+console.log(roundsPlayed);
+
+
+
+
+
+
+
+}
